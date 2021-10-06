0f23d5093eca701879ac43bd7fdeb920be642e64
darylduck <daryl.duckmanton@gmail.com>
2021-10-06 21:41:19 +1100

    Latest

--- src/components/Ticker/Ticker.js
+++ src/components/Ticker/Ticker.js
@@ -1,14 +1,30 @@
+import { intervalToDuration, isBefore } from 'date-fns';
+
+
 import { TickerCell } from 'components/TickerCell';
 import { TickerSeparator } from 'components/TickerSeparator';
 
 import style from './Ticker.module.scss';
 
-export const Ticker = () => {
-    const days = 6;
-    const hours = 22;
-    const minutes = 40;
-    const seconds = 0;
-    const isTimeUp = false;
+export const Ticker = ({ futureDate }) => {
+    const now = new Date();
+    const isTimeUp = isBefore(futureDate, now);
+    let days = 0; 
+    let hours = 0;
+    let minutes = 0;
+    let seconds = 0;
+
+    if (!isTimeUp) {
+        const duration = intervalToDuration({
+            start: now,
+            end: futureDate
+        });
+
+        days = duration.days;
+        hours = duration.days;
+        minutes = duration.days;
+        seconds = duration.days;
+    }
 
     const tickerContents = isTimeUp ? (
         <div className={ style.timeIsUp }>Time is up!!!</div>
